198f992d83767811b3f30bcbb4abdf94e4e6c3f5
LeinneSW <nno88551@naver.com>
2022-03-31 11:42:41 +0900

    Added DBManager

---
+++ src/leinne/java/sudoku/db/DBManager.java
@@ -0,0 +1,31 @@
+package leinne.java.sudoku.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager{
+
+    private Connection connection = null;
+
+    public void connect(){
+        try{
+            var conn = DriverManager.getConnection("jdbc:mariadb://todo.net:3306/sudoku", "todo", "todo");
+            if(conn != null){
+                connection = conn;
+            }
+        }catch(SQLException e){}
+    }
+
+    public boolean isClosed(){
+        try{
+            return connection == null || connection.isClosed();
+        }catch(SQLException e){
+            return true;
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
