551d4cb4ad7d408e6ded3786ae6ae350343b8b3d
LeinneSW <nno88551@naver.com>
2022-04-01 15:20:48 +0900

    oops

--- src/leinne/java/sudoku/Main.java
+++ src/leinne/java/sudoku/Main.java
@@ -1,10 +1,11 @@
 package leinne.java.sudoku;
 
+import leinne.java.sudoku.ui.window.LoginWindow;
 import leinne.java.sudoku.ui.window.WindowManager;
 
 public class Main{
 
     public static void main(String[] args){
-        WindowManager.getInstance().getLoginWindow().setVisible(true);
+        WindowManager.getInstance().setCurrentWindow(new LoginWindow());
     }
 }
