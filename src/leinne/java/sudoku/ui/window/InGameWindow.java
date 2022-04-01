982eb3f7f2eb7fb8133793f69a075fdc4c6d8e46
LeinneSW <nno88551@naver.com>
2022-04-02 04:06:59 +0900

    Improved timer to display down to milliseconds and added stop and reset buttons.

--- src/leinne/java/sudoku/ui/window/InGameWindow.java
+++ src/leinne/java/sudoku/ui/window/InGameWindow.java
@@ -4,15 +4,14 @@ import leinne.java.sudoku.SudokuSystem;
 import leinne.java.sudoku.ui.component.GamePanel;
 
 import java.awt.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import javax.swing.*;
 
 public class InGameWindow extends Window{
 
-    private static final String TIMER_FORMAT = "%02d:%02d:%02d";
+    private static final String TIMER_FORMAT = "%02d:%02d:%02d'%02d              ";
 
-    private final Timer timer = new Timer(1000, null);
-    private final AtomicInteger time = new AtomicInteger();
+    private long startTime;
+    private final Timer timer = new Timer(20, null);
     private final GamePanel gamePanel = new GamePanel(new GridLayout(9, 9, 0, 0));
 
     protected void initDesign(){
@@ -24,25 +23,39 @@ public class InGameWindow extends Window{
         main.add(gamePanel, BorderLayout.CENTER);
 
         // create timer, menu button
-        var lblTimer = new JLabel("00:00:00");
+        var lblTimer = new JLabel("00:00:00'00              ");
         lblTimer.setFont(new Font("Dialog", Font.BOLD, 32));
         topPanel.add(lblTimer);
 
         timer.addActionListener((event) -> {
-            var t = time.incrementAndGet();
-            lblTimer.setText(String.format(TIMER_FORMAT, t / 3600, t / 60, t % 60));
+            var time = getTime();
+            var second = time / 100;
+            lblTimer.setText(String.format(TIMER_FORMAT, second / 3600, second / 60 % 60, second % 60, time % 100));
         });
-        timer.start();
+
+        var btnClear = new JButton("초기화");
+        btnClear.addActionListener((event) -> {
+            for(var tile : gamePanel.getNumberTiles()){
+                if(!tile.isProblemNumber()){
+                    tile.setNumber(0, false);
+                }
+            }
+        });
+        topPanel.add(btnClear);
+
+        var btnStop = new JButton("중단");
+        btnStop.addActionListener((event) -> SudokuSystem.getInstance().getWindowManager().setCurrentWindow(new SelectProblemWindow()));
+        topPanel.add(btnStop);
     }
 
     public void open(){
         super.open();
+        startTime = System.nanoTime();
         timer.start();
     }
 
     public void close(){
         super.close();
-        time.set(0);
         timer.stop();
     }
 
@@ -50,8 +63,8 @@ public class InGameWindow extends Window{
         SudokuSystem.getInstance().getWindowManager().setCurrentWindow(new SelectProblemWindow());
     }
 
-    public int getTime(){
-        return time.get();
+    public long getTime(){
+        return (System.nanoTime() - startTime) / 10000000;
     }
 
     public GamePanel getGamePanel(){
